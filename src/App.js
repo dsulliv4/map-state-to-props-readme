24e068bfbb07bc335c42080d79f72537d056cd66
dsulliv4 <dsulliv4@binghamton.edu>
2021-12-25 23:25:10 -0500

    map state to props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-	handleOnClick = () => {
-		this.props.store.dispatch({
-			type: 'INCREASE_COUNT',
-		});
-	}
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
-			</div>
-		);
-	}
-}
+  handleOnClick() {
+    this.props.dispatch({
+      type: 'INCREASE_COUNT',
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={(event) => this.handleOnClick()}>
+          Click
+        </button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+};
 
 export default App;
